408b95a87d59cfd38e173cbf28b40c1a24d9e755
Jean-Mathieu Potvin <jm@potvin.xyz>
2022-12-16 16:51:39 -0500

    Initial commit on random utility functions (WIP) [CU-860pa78bj].

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
 collapse <- function(x = character()) {
     return(paste0(x, collapse = ""))
 }
+
+classes <- function(x) {
+    return(vapply(x, \(el) class(el)[[1L]], NA_character_, USE.NAMES = FALSE))
+}
+
+typeofs <- function(x) {
+    return(vapply(calls, typeof, NA_character_, USE.NAMES = FALSE))
+}
