f9598a288bde8d7203335ff911cc5143bc132a39
Jean-Mathieu Potvin <jm@potvin.xyz>
2022-12-20 17:41:55 -0500

    Document function `analyze()` [CU-860pa78bj].

--- R/analyze.R
+++ R/analyze.R
@@ -1,3 +1,69 @@
+#' Extract text from source code
+#'
+#' Statically [analyze()] \R scripts (without executing the source code) and
+#' extract character strings to be translated.
+#'
+#' @param file A file path. It must point to an existing \R script.
+#'
+#' @details
+#' The internal engine relies on [base::parse()] to decompose \R code into
+#' sequences of *tokens* (single elements of the language). For more information
+#' on \R tokens, see [utils::getParseData()].
+#'
+#' @note
+#' What follows describes the steps that [analyze()] goes through each time it
+#' is called. **Users typically do not need to bother with these details.**
+#'
+#'   1. Contents of `file` is first transformed into a sequence of tokens by
+#'      [tokenize()]. Such a sequence is internally called a *stream*. Empty
+#'      strings representing `expr` tokens are conveniently removed.
+#'
+#'   2. Calls (`'('` tokens) made to [translate()] in the `stream` are located
+#'      by [findCalls()]. This function extracts indices of the underlying
+#'      `SYMBOL_FUNCTION_CALL` tokens.
+#'
+#'   3. For each index returned by [findCalls()], the `stream` is traversed by
+#'      [findCallEnd()] to locate where each call ends. It does so by attempting
+#'      to detect a matching outer `')'` token.
+#'
+#'   4. The stream is split into many sub-streams according to what [findCalls()]
+#'      and [findCallEnd()] returned. Each sub-stream is parsed as an unevaluated
+#'      [call][base::call()] object by [parseStream()].
+#'
+#'   5. The actual value passed to argument `text` is extracted from each call
+#'      by function [extractCallArgumentValue()].
+#'
+#'   6. The extracted values are checked. Since [analyze()] performs a
+#'      static code analysis, these must be litteral character strings.
+#'
+#' With further work, this limitation could be lifted (for some cases) in the
+#' future.
+#'
+#' @returns
+#' A named list of length 6 containing the following elements:
+#'
+#' \describe{
+#'   \item{`file`}{Path to the underlying \R script. See `file` above.}
+#'   \item{`workingDir`}{Current working directory.}
+#'   \item{`encoding`}{Encoding of `file`.}
+#'   \item{`timeStamp`}{Last time `file` was modified in Coordinated Universal
+#'     Time (UTC).}
+#'   \item{`nCalls`}{Number of detected calls to [translate()].}
+#'   \item{`text`}{Each element in `text` is a named list of length 2
+#'     containing the following elements:
+#'
+#'   \describe{
+#'     \item{`location`}{The `location` of a call to [translate()] in file.
+#'       The format is `"Ln X, Col Y"`, where `X` and `Y` are both positive
+#'       integers.}
+#'     \item{`value`}{The actual value passed to formal argument `text` of
+#'       [translate()] either by name (preferred) or by position.}
+#'   }}
+#' }
+#'
+#' @author Jean-Mathieu Potvin (<jm@@potvin.xyz>)
+#'
+#' @keywords internal
 analyze <- function(file = character(1L)) {
     stream <- tokenize(file)
     meta   <- attributes(stream)[c(
@@ -9,7 +75,7 @@ analyze <- function(file = character(1L)) {
     # findCalls() returns the positions of the
     # matching '(' tokens. The function's name
     # that precedes it must be included.
-    starts    <- findCalls(stream, "translate") - 1L
+    starts    <- findCalls(stream) - 1L
     ends      <- vapply1i(starts, findCallEnd, stream = stream)
     locations <- attr(stream, "locations")[starts]
 
@@ -20,7 +86,7 @@ analyze <- function(file = character(1L)) {
 
     # Extract value passed to argument
     # text from calls to translate().
-    values   <- lapply(calls, extractCallArgumentValue, "text", 1L)
+    values   <- lapply(calls, extractCallArgumentValue)
     isNotChr <- vapply1c(values, class) != "character"
 
     if (any(isNotChr)) {
@@ -34,6 +100,12 @@ analyze <- function(file = character(1L)) {
     return(c(meta, nCalls = length(calls), list(text = text)))
 }
 
+
+# Low-level engine -------------------------------------------------------------
+
+
+# FIXME: document me in analyze-engine.Rd.
+# FIXME: mark me as an internal function.
 tokenize <- function(file = character(1L)) {
     assertString(file)
 
@@ -61,8 +133,11 @@ tokenize <- function(file = character(1L)) {
             locations  = srclocs))
 }
 
-findCalls <- function(stream = character(), name = character(1L)) {
+# FIXME: document me in analyze-engine.Rd.
+# FIXME: mark me as an internal function.
+findCalls <- function(stream = character(), name = "translate") {
     assertNonEmptyCharacter(stream)
+    assertNoEmptyStrings(stream)
     assertString(name)
 
     # Calls are identified by a '(' token. In
@@ -72,8 +147,11 @@ findCalls <- function(stream = character(), name = character(1L)) {
     return(pos[stream[pos - 1L] == name])
 }
 
+# FIXME: document me in analyze-engine.Rd.
+# FIXME: mark me as an internal function.
 findCallEnd <- function(stream = character(), start = integer(1L)) {
     assertNonEmptyCharacter(stream)
+    assertNoEmptyStrings(stream)
     assertScalarInteger(start)
 
     nTokens <- length(stream)
@@ -125,16 +203,17 @@ findCallEnd <- function(stream = character(), start = integer(1L)) {
     return(0L)
 }
 
+# FIXME: document me in analyze-engine.Rd.
+# FIXME: mark me as an internal function.
 parseStream <- function(stream = character()) {
     assertNonEmptyCharacter(stream)
+    assertNoEmptyStrings(stream)
     return(str2lang(paste0(stream, collapse = "")))
 }
 
-extractCallArgumentValue <- function(
-    call    = call(),
-    argName = character(1L),
-    argPos  = integer(1L))
-{
+# FIXME: document me in analyze-engine.Rd.
+# FIXME: mark me as an internal function.
+extractCallArgumentValue <- function(call, argName = "text", argPos  = 1L) {
     assertString(argName)
     assertScalarInteger(argPos)
 
