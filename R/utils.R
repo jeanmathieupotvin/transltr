7ec52879294753a63bc072182e9ed115447d8cc6
Jean-Mathieu Potvin <jm@potvin.xyz>
2022-12-20 14:48:47 -0500

    Removed unused functions `typeofs()` and `classes()`. Add new functions `map(), `vapply1i()`, and `vapply1c()` [CU-860pa78bj].

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,11 @@
-classes <- function(x) {
-    return(vapply(x, \(el) class(el)[[1L]], NA_character_, USE.NAMES = FALSE))
+map <- function(fun, ..., moreArgs = list()) {
+    return(.mapply(fun, list(...), moreArgs))
 }
 
-typeofs <- function(x) {
-    return(vapply(calls, typeof, NA_character_, USE.NAMES = FALSE))
+vapply1i <- function(x, fun, ..., useNames = FALSE) {
+    return(vapply(x, fun, NA_integer_, ..., USE.NAMES = useNames))
+}
+
+vapply1c <- function(x, fun, ..., useNames = FALSE) {
+    return(vapply(x, fun, NA_character_, ..., USE.NAMES = useNames))
 }
