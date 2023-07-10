1f891b40640382c0d8bfdd14f4bcaf7d2621aad6
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2023-07-10 14:18:41 -0400

    Removing older assets. Will be reintroduced [CU-860r9xmj7].

--- tests/testthat/_snaps/stopf.md
+++
@@ -1,4 +0,0 @@
-# stopf() validates type
-
-    'arg' should be one of "TypeError", "InterfaceError"
-
--- tests/testthat/test-stopf.R
+++
@@ -1,8 +0,0 @@
-test_that("stopf() throws error", {
-    expect_error(stopf("TypeError", "argument must contain %i values.", 7L))
-})
-
-test_that("stopf() validates type", {
-    expect_error(stopf("ErrorInjected"))
-    expect_snapshot_error(stopf("ErrorInjected"))
-})
