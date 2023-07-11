c8c4ba92ce36936391047c32db081bb56be691c3
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2023-07-11 16:51:02 -0400

    New placeholder for function translate() [CU-860r9xmj7].
    This placeholder is required even if it is not implemented yet because its definition is required by (upcoming) internal mechanism getTranslationsFromExpression(). See next commit for it.

---
+++ R/translate.R
@@ -0,0 +1,11 @@
+#' Future work
+#'
+#' Included because required by [findTranslationsInExpression()]. To be
+#' implemented.
+#'
+#' @author Jean-Mathieu Potvin (<jeanmathieupotvin@@ununoctium.dev>)
+#'
+#' @export
+translate <- function(..., lang = "en", translator = NULL) {
+    return(.NotYetImplemented())
+}
