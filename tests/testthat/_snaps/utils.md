fb4a723f270d527a5c16fe3445a126bdd02b74fd
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2023-08-03 10:52:32 -0400

    New dummy temp function to trick roxygen2 [CU-860rafnru].

---
+++ tests/testthat/_snaps/utils.md
@@ -0,0 +1,4 @@
+# trickRoxyygenTemporarily() throws an error until it is replaced
+
+    'trickRoxyygenTemporarily' is not implemented yet
+
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,9 @@
+test_that("trickRoxyygenTemporarily() throws an error until it is replaced", {
+    # FIXME: replace me by next misc function.
+    expect_error(trickRoxyygenTemporarily())
+    expect_snapshot_error(trickRoxyygenTemporarily())
+})
+
 test_that("%||% works", {
     expect_identical(NULL %||% 2L, 2L)
     expect_identical(1L   %||% 2L, 1L)
