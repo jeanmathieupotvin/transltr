da58ffc7134da4a4dd0e600e6048e75f31817f88
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-02-10 11:23:27 -0500

    Initial commit on .match.arg() [CU-8678ran8h].
    This is a refactoring of base::match.arg() that better suits my needs. I dislike the error messages returned by the former.

--- R/utils.R
+++ R/utils.R
@@ -38,6 +38,44 @@ trimParsedExpr <- function(expr, width = integer(1L)) {
     return(expr)
 }
 
+# This is a refactoring of base::match.arg() with a transtlr
+# touch on it that enforces single values by defaut. It must
+# be called within a parent function. This is what it means
+# to 'match an argument'.
+.match.arg <- function(arg = "") {
+    caller     <- getCallerName(-2L)
+    stackIndex <- sys.parent()
+    formalArgs <- formals(sys.function(stackIndex))
+    argName    <- as.character(substitute(arg))
+    choices    <- eval(formalArgs[[argName]], sys.frame(stackIndex))
+
+    # If arg is identical to choices, this implies
+    # user did not specify a specific value and is
+    # requesting the default one.
+    if (identical(arg, choices)) {
+        return(choices[[1L]])
+    }
+
+    if (!isString(arg)) {
+        halt("'%s' must be a non-NA character of length 1. It can be empty.",
+            argName,
+            caller = caller)
+    }
+
+    if ({ matchIndex <- pmatch(arg, choices, nomatch = 0L) } == 0L) {
+        # This creates a numbered list of choices
+        # to be printed as part of the error below.
+        listedChoices <- sprintf(" [%i] '%s'\n", seq_along(choices), choices)
+
+        halt("'%s' must be equal to one of the listed value below.\n%s",
+            argName,
+            paste0(listedChoices, collapse = ""),
+            caller = caller)
+    }
+
+    return(choices[[matchIndex]])
+}
+
 `%?%` <- function(lhs, rhs) {
     return(if (nzchar(lhs)) lhs else rhs)
 }
