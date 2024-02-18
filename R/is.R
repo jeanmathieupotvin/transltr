6e0c0412950bd55b05436beffa878001c2efe5d1
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-02-18 10:37:04 -0500

    Rermove checks on min and max [CU-8678ran8h].
    This should be the responsibility of the developer.

--- R/is.R
+++ R/is.R
@@ -11,10 +11,6 @@ isSingleLgl <- function(x) {
 }
 
 isSingleIntInRange <- function(x, min = -max, max = .Machine$integer.max) {
-    if (min > max) {
-        halt("'min' must be lower than or equal to 'max'.")
-    }
-
     return(is.integer(x) && length(x) == 1L && !is.na(x) && x >= min && x <= max)
 }
 
@@ -23,9 +19,5 @@ isSingleDblInRange <- function(
     min = .Machine$double.xmin,
     max = .Machine$double.xmax)
 {
-    if (min > max) {
-        halt("'min' must be lower than or equal to 'max'.")
-    }
-
     return(is.double(x) && length(x) == 1L && !is.na(x) && x >= min && x <= max)
 }
