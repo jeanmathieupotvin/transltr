8c536ffcb3df836f33926b3aa55874c20cc26491
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-08-28 08:28:45 -0400

    Initial commit on utility vapply_1*().
    They wrap vapply() and sets a default return value.

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
 `%??%` <- function(lhs, rhs) {
     return(if (is.null(lhs)) rhs else lhs)
 }
+
+vapply_1l <- function(x, fun, ...) {
+    return(vapply(x, fun, NA, ..., USE.NAMES = FALSE))
+}
+
+vapply_1c <- function(x, fun, ...) {
+    return(vapply(x, fun, NA_character_, ..., USE.NAMES = FALSE))
+}
