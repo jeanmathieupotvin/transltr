7d7264fce58ffae2bf3725773f46e7221ee5e6bc
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-08-28 14:14:32 -0400

    Small internal changes to how assert_*() processes err_msg.
    Also reorganized contents of script prior to incoming documentation and tests.

--- R/assert.R
+++ R/assert.R
@@ -1,31 +1,55 @@
+is_chr <- function(x, accept_empty = FALSE) {
+    return(is.character(x) && (length(x) || accept_empty) && !anyNA(x))
+}
+
+is_int1 <- function(x) {
+    return(is.integer(x) && length(x) == 1L && !is.na(x))
+}
+
+is_chr1 <- function(x, accept_empty_string = FALSE) {
+    return(
+        is.character(x) &&
+        length(x) == 1L &&
+        (nzchar(x) || accept_empty_string) &&
+        !is.na(x))
+}
+
+is_list <- function(x, accept_empty = FALSE) {
+    return(is.list(x) && (length(x) || accept_empty))
+}
+
+is_named <- function(x, accept_empty_names = FALSE, accept_na_names = FALSE) {
+    if (length(x)) {
+        x_names <- names(x)
+
+        return(
+            !is.null(x_names) &&
+            (all(nzchar(x_names)) || accept_empty_names) &&
+            (!anyNA(x_names) || accept_na_names))
+    }
+
+    # Our convention is to consider empty
+    # vectors as being properly named.
+    return(TRUE)
+}
+
 assert_chr <- function(
     x            = character(),
     accept_empty = FALSE,
     throw_error  = TRUE)
 {
+    err_msg <- ""
+
     if (!is_chr(x, accept_empty)) {
         err_msg <- sprintf(
-            "'%s' must be a character vector of non-NA values.",
-            deparse(substitute(x)))
+            "'%s' must be a%s character vector of non-NA values.",
+            deparse(substitute(x)),
+            if (accept_empty) "" else " non-empty")
 
         if (throw_error) stops(err_msg)
-        return(err_msg)
     }
 
-    return("")
-}
-
-assert_int1 <- function(x = 0L, throw_error = TRUE) {
-    if (!is_int1(x)) {
-        err_msg <- sprintf(
-            "'%s' must be a non-NA integer of length 1.",
-            deparse(substitute(x)))
-
-        if (throw_error) stops(err_msg)
-        return(err_msg)
-    }
-
-    return("")
+    return(err_msg)
 }
 
 assert_chr1 <- function(
@@ -33,17 +57,47 @@ assert_chr1 <- function(
     accept_empty_string = FALSE,
     throw_error         = TRUE)
 {
+    err_msg <- ""
+
     if (!is_chr1(x, accept_empty_string)) {
         err_msg <- sprintf(
-            "'%s' must be a non-NA character of length 1.%s",
+            "'%s' must be a non-NA%s character of length 1.",
             deparse(substitute(x)),
-            if (accept_empty_string) " It can be empty an empty string." else "")
+            if (accept_empty_string) "" else " and non-empty")
 
         if (throw_error) stops(err_msg)
-        return(err_msg)
     }
 
-    return("")
+    return(err_msg)
+}
+
+assert_int1 <- function(x = 0L, throw_error = TRUE) {
+    err_msg <- ""
+
+    if (!is_int1(x)) {
+        err_msg <- sprintf(
+            "'%s' must be a non-NA integer of length 1.",
+            deparse(substitute(x)))
+
+        if (throw_error) stops(err_msg)
+    }
+
+    return(err_msg)
+}
+
+assert_list <- function(x = list(), accept_empty = FALSE, throw_error = TRUE) {
+    err_msg <- ""
+
+    if (!is_list(x, accept_empty)) {
+        err_msg <- sprintf(
+            "'%s' must be a%s list.",
+            deparse(substitute(x)),
+            if (accept_empty) "" else " non-empty")
+
+        if (throw_error) stops(err_msg)
+    }
+
+    return(err_msg)
 }
 
 assert_names <- function(
@@ -52,18 +106,19 @@ assert_names <- function(
     accept_na_names    = FALSE,
     throw_error        = TRUE)
 {
+    err_msg <- ""
+
     if (!is_named(x, accept_empty_names, accept_na_names)) {
         err_msg <- sprintf(
-            "'%s' must have valid names. They cannot be NULL.%s%s",
+            "'%s' must have names.%s%s",
             deparse(substitute(x)),
             if (accept_empty_names) " They can be empty strings." else "",
             if (accept_na_names)    " They can be NA values."     else "")
 
         if (throw_error) stops(err_msg)
-        return(err_msg)
     }
 
-    return("")
+    return(err_msg)
 }
 
 # This is a refactoring of base::match.arg().
@@ -93,39 +148,4 @@ assert_choice <- function(x, quote_values = FALSE, throw_error = TRUE) {
 
     assign(a_name, a_value, parent)
     return("")
-}
-
-
-# is_*() functions -------------------------------------------------------------
-
-
-is_chr <- function(x, accept_empty = FALSE) {
-    return(is.character(x) && (length(x) || accept_empty) && !anyNA(x))
-}
-
-is_int1 <- function(x) {
-    return(is.integer(x) && length(x) == 1L && !is.na(x))
-}
-
-is_chr1 <- function(x, accept_empty_string = FALSE) {
-    return(
-        is.character(x) &&
-        length(x) == 1L &&
-        (nzchar(x) || accept_empty_string) &&
-        !is.na(x))
-}
-
-is_named <- function(x, accept_empty_names = FALSE, accept_na_names = FALSE) {
-    if (length(x)) {
-        x_names <- names(x)
-
-        return(
-            !is.null(x_names) &&
-            (all(nzchar(x_names)) || accept_empty_names) &&
-            (!anyNA(x_names) || accept_na_names))
-    }
-
-    # Our convention is to consider empty
-    # vectors as being properly named.
-    return(TRUE)
 }
