24945956a01c7f6aa95461d54e4a9a69d9bff776
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-10-06 12:17:19 -0400

    Revamped translate() interface (WIP).
    This commit also introduces S3 generics is_translate_call() and
    match_translate_call() to identify and process calls to transltr::translate().
    
    While is_translate_call() is fast enough, I am well aware that relying on
    regex to parse R code is not the best. For now, it works fine and it is
    fast enough. However, in the future, I would really like to write an
    optimized tokenizer/parser that could further handle R code such as
    call("translate") and do.call(translate, list(...)). This is not important
    for now.

--- R/translate.R
+++ R/translate.R
@@ -3,6 +3,43 @@
 #' Placeholder for future function [translate()].
 #'
 #' @export
-translate <- function() {
+translate <- function(..., lang = get_language(), concat = " ", key = "en") {
     return(.NotYetImplemented())
 }
+
+is_translate_call <- function(x, ...) {
+    UseMethod("is_translate_call")
+}
+
+#' @export
+is_translate_call.character <- function(x, ...) {
+    return(grepl("^(['\"]?transltr['\"]?::)?['\"]?translate['\"]?\\((.*?)\\)$", x))
+}
+
+match_translate_call <- function(x, ...) {
+    UseMethod("match_translate_call")
+}
+
+#' @export
+match_translate_call.character <- function(x, ...) {
+    return(match_translate_call.call(str2lang(x), ...))
+}
+
+#' @export
+match_translate_call.call <- function(x, ...) {
+    # We do not check whether the call object truly calls
+    # translate() because this check should be done prior
+    # to calling match_translate_call().
+    cl        <- match.call(transltr::translate, x, expand.dots = FALSE)
+    cl$concat <- x$concat %??% .TRANSLATE_FORMAL_CONCAT
+    cl$key    <- x$key    %??% .TRANSLATE_FORMAL_KEY
+    return(cl)
+}
+
+# Not passed to translate() because
+# internal constants should be kept
+# hidden from users. However, these
+# constants must be in sync with
+# formal args of translate().
+.TRANSLATE_FORMAL_CONCAT <- formals(translate)$concat
+.TRANSLATE_FORMAL_KEY    <- formals(translate)$key
