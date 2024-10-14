4d7f2b9bd89cdc6e4f8ed81f7ac941c051eda3fc
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-10-14 14:04:04 -0400

    Add FIXME note on *_translator() functions.

--- R/set-translator.R
+++ R/set-translator.R
@@ -1,8 +1,11 @@
-#' Placeholder
-#'
-#' Placeholder for future functions [set_translator()] and [get_translator()].
-#'
-#' @rdname set-translator
+# FIXME: the following implementation are temporary. Not that they are bad,
+# but they do not incorporate a namespacing concept. There is a single (global)
+# chain of Translator objects which breaks R usual encapsulation mechanisms
+# for packages. Instead, it should handle one chain per package and default to
+# a global chain when not in "package mode". This will be fixed later, as the
+# current focus is on class Translator and function translate().
+
+
 #' @export
 set_translator <- function(x = translator()) {
     if (!is.null(x) && !is_translator(x)) {
@@ -14,7 +17,6 @@ set_translator <- function(x = translator()) {
     return(invisible())
 }
 
-#' @rdname set-translator
 #' @export
 get_translator <- function() {
     # sys.nframe() counts call to get_translator(), but
