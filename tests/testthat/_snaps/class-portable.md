ed7dc6576d3feeafa084272643720b6471c8c37d
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-11-27 13:03:29 -0500

    Tests for portable() and is_location().

---
+++ tests/testthat/_snaps/class-portable.md
@@ -0,0 +1,16 @@
+# portable() validates super
+
+    Code
+      portable(1L, super = 1L)
+    Condition
+      Error:
+      ! 'super' must be a character vector of non-NA values.
+
+# portable() validates tag if not null
+
+    Code
+      portable(1L, tag = 1L)
+    Condition
+      Error:
+      ! 'tag' must be a non-NA and non-empty character of length 1.
+
--- tests/testthat/test-class-portable.R
+++ tests/testthat/test-class-portable.R
@@ -1,3 +1,43 @@
+# portable() -------------------------------------------------------------------
+
+
+test_that("portable() returns x", {
+    expect_identical(portable(1L), 1L, ignore_attr = TRUE)
+})
+
+test_that("portable() returns a Portable object", {
+    expect_s3_class(portable(1L), "Portable")
+})
+
+test_that("portable() validates super", {
+    expect_error(portable(1L, super = 1L))
+    expect_snapshot(portable(1L, super = 1L), error = TRUE)
+})
+
+test_that("portable() validates tag if not null", {
+    expect_error(portable(1L, tag = 1L))
+    expect_snapshot(portable(1L, tag = 1L), error = TRUE)
+})
+
+test_that("portable() adds a tag attribute if not null", {
+    expect_identical(attr(portable(1L, tag = "Integer"), "tag"), "Integer")
+})
+
+test_that("portable() adds super-classes if any", {
+    super_classes <- c("SuperPortableInteger", "PortableInteger")
+    expect_s3_class(portable(1L, super_classes), super_classes)
+})
+
+
+# is_portable() ----------------------------------------------------------------
+
+
+test_that("is_portable() returns a logical", {
+    expect_true(is_portable(portable(1L)))
+    expect_false(is_portable(1L))
+})
+
+
 # Degenerate as_*() methods ----------------------------------------------------
 
 
