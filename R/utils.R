515441acbf0f2a9a5b5b1df59d1a54836663bbfa
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-12-01 21:16:01 -0500

    Move utc() to script utils.R.

--- R/utils.R
+++ R/utils.R
@@ -46,6 +46,27 @@ map <- function(fun, ..., moreArgs = list()) {
 }
 
 
+#' Get Current Date and Time
+#'
+#' Get the current Coordinated Universal Time (UTC).
+#'
+#' @details
+#' The format is
+#'
+#' ```r
+#' "<Full Month> <Day>, <Year> @ <Hours>:<Minutes>:<Seconds> (UTC)".
+#' ```
+#'
+#' @returns A character string.
+#'
+#' @rdname utc
+#' @family utility functions
+#' @keywords internal
+utc <- function() {
+    return(format(Sys.time(), tz = "UTC", format = "%B %d, %Y @ %T (%Z)"))
+}
+
+
 #' Throw Errors
 #'
 #' @description
