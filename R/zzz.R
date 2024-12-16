035c3f8911f50a63140be922a5530255b248200c
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-12-16 16:33:38 -0500

    Bugfix: encapsulate call to options() in a hook.
    Otherwise, it won't work, and option transltr.default.path is not registered.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
-options(
-    # Default path to the main Portable Translator File (PTF).
-    transltr.default.path = file.path("inst", "transltr", "_translator.yml"))
+# Hooks ------------------------------------------------------------------------
+
+
+.onLoad <- function(libname, pkgname) {
+    options(
+        # Default path to the main Portable Translator File (PTF).
+        transltr.default.path = file.path("inst", "transltr", "_translator.yml"))
+}
