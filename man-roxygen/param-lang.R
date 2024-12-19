460f3863f1cd51e5a6436aec67aa30a2a32900b5
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-12-19 15:31:07 -0500

    Update doc of parameter lang.
    It is shared, and has a dedicated roxygen2 template. This yield a couple of
    changes in many files.

--- man-roxygen/param-lang.R
+++ man-roxygen/param-lang.R
@@ -5,5 +5,5 @@
 #'   language name. While users retain full control over codes, it is best
 #'   to use language codes stemming from well-known schemes such as
 #'   [IETF BCP 47](https://en.wikipedia.org/wiki/IETF_language_tag), or
-#'   [ISO 639-1](https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes).
-#'   Doing so maximizes portability and cross-compatibility between packages.
+#'   [ISO 639-1](https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes)
+#'   to maximize portability and cross-compatibility.
