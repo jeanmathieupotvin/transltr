11d35409e2af09e376c943db4a0800eb347efc28
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2024-12-19 15:34:41 -0500

    Major revamp of translator_write() and of its documentation.
    translator_write() now incorporates the logic of deprecated function
    crate_parent_dir(). It handles paths much more consistently, add allows
    more flexibility in how users can change field Translations Files of an
    ExportedTranslator (Portable Translator).
    
    translator_write() further gets a new overwrite argument that is false
    by default to prevent data loss.

--- R/translator-io.R
+++ R/translator-io.R
@@ -1,90 +1,61 @@
 #' Read and Write Translations
 #'
-#' @description
-#' Read (import) *Portable Translator/Translations Files*, and convert them
-#' to [`Translator`][Translator] objects.
-#'
-#' Write (export) [`Translator`][Translator] objects as
-#' *Portable Translators/Translations Files*.
+#' Export [`Translator`][Translator] objects to text files, and import such
+#' files back into \R as [`Translator`][Translator] objects.
 #'
 #' @details
-#' [translator_write()] creates two types of file: a single
-#' *Portable Translator File*, and further *Portable Translations Files*. Both
-#' can be referred to as `PTF` files, and jointly represents all the information
-#' contained within a [`Translator`][Translator] object. As such, PTF files are
-#' closely tied by design, work together, but target different audiences:
+#' To ease collaboration and maintenance, the information contained within
+#' a [`Translator`][Translator] object is split. Translations are exported
+#' independently from other fields. Two types of objects are created from
+#' a [`Translator`][Translator] object: a single *Portable Translator*, and
+#' zero, or more *Portable Translations*. These objects are portable because
+#' they are textual representations that can be inspected, and modified using
+#' a wide variety of tools and systems.
 #'
-#'   * Portable Translator Files are useful to developers, and
-#'   * Portable Translations Files are useful to non-technical collaborators
+#' Portable objects are stored in plain text files. The inner workings of the
+#' serialization process are thoroughly described in [serialize()].
+#'
+#' [translator_write()] creates two types of file: one file containing a
+#' Portable Translator, and further file(s) containing Portable Translations.
+#' They target different audiences:
+#'
+#'   * Portable Translator files are useful to **developers**, and
+#'   * Portable Translations files are useful to **non-technical collaborators**
 #'     such as translators.
 #'
-#' ## Portable Translator File
+#' ## Portable Translator
 #'
-#' A Portable Translator File (PTF) is a human-friendly, cross language,
-#' and textual representation (serialization) of a [`Translator`][Translator]
-#' object. The format heavily relies on [YAML 1.1](https://yaml.org/spec/1.1/),
-#' a popular data serialization format. However, users do not actually have to
-#' know anything about YAML to read, or write [`Translator`][Translator]
-#' objects.
+#' A Portable Translator is a [YAML 1.1](https://yaml.org/spec/1.1/)
+#' representation of a [`Translator`][Translator] object stripped of all
+#' its translations except those that are registered as source text. It
+#' references related Portable Translations files via a `Translations Files`
+#' field.
 #'
-#' Portable Translator Files are snapshots of [`Translator`][Translator]
-#' objects. To ease collaboration and maintenance, translations are grouped by
-#' language, and stored in separate Portable Translations Files). These files
-#' are listed and referenced by the `Translations Files` field of a Portable
-#' Translator File.
+#' ## Portable Translations
 #'
-#' ## Portable Translations File
+#' Translations registered in a [`Translator`][Translator] object are extracted,
+#' grouped by language, and stored in distinct file(s). One
+#' Portable Translations is created for each non-source native language
+#' registered in `x`. Fore more information, see
+#' [`Translator$native_languages`][Translator].
 #'
-#' A Portable Translations File (PTF) is a human-friendly, cross language,
-#' and textual representation (serialization) of the translations contained
-#' by a [`Translator`][Translator] object. It has two parts:
+#' A Portable Translations is a [FLAT 1.0][flat_serialize()] representation.
+#' This format is custom, and attempts to be as simple as possible for
+#' non-technical collaborators.
 #'
-#'   1. a YAML header containing basic information on source and target
-#'        languages, and
-#'   2. a sequence of unindented (flat) and named sections.
+#' @param path A non-empty and non-[NA][base::NA] character string. A path to
+#'   a file to read from, or write to.
 #'
-#' See [export()] for more information.
+#'   * This file must be a Portable Translator file for [translator_read()].
+#'   * This file must be a Portable Translations file for [translations_read()].
 #'
-#' Note that [translator_write()] never creates a Portable Translations File
-#' for the source language itself. Attempting to translate the source text to
-#' itself makes no sense.
-#'
-#' ## Working with PTF files
-#'
-#' The easiest way to import translations is to use [translator_read()]. This
-#' function is designed to read all related PTFs, extract their contents, and
-#' construct a suitable [`Translator`][Translator] object from it. This is the
-#' preferred interface. The same is true for [translator_write()].
-#'
-#' Users may read, or write individual Portable Translations Files with
-#' [translations_read()], and [translations_write()], respectively. This
-#' can be useful for debugging purposes. See Examples below.
-#'
-#' ## Encodings
-#'
-#' **The preferred encoding is (and should always be) UTF-8.** If another
-#' encoding must be used for some obscure reason, the `encoding` argument
-#' has to be updated. Encodings should be known, never inferred.
-#'
-#' [translator_write()], and [translations_write()] enforces UTF-8. This
-#' cannot be changed.
-#'
-#' ## Requirements
-#'
-#' To export a [`Translator`][Translator], the underlying registered
-#' [`Text`][Text] objects must have the same source language (the same
-#' [`Text$source_lang`][Text] value). An error is thrown otherwise.
-#'
-#' @param path A character string. A path to a file to read from, or write to.
-#'   Its parent directories are automatically created using [dir.create()] if
-#'   they do not exist.
-#'
-#'   By default, [translator_read()], and [translator_write()] respectively
-#'   reads from, and writes to a default location given by global \R option
-#'   `transltr.default.path`. It points to a standard
-#'   **Portable Translator File**. See Details below.
+#'   See Details for more information. [translator_write()] automatically
+#'   creates the parent directory of `path` (recursively) if it does not exist.
 #'
 #' @param tr A [`Translator`][Translator] object.
+#'
+#' @param overwrite A non-[NA][base::NA] logical value. Should existing
+#'   *Portable Translator* files (pointed to by `path`) be overwritten?
 #'
 #' @template param-encoding
 #'
@@ -95,36 +66,38 @@
 #' [`Translator`][Translator].
 #'
 #' [translator_write()] returns `NULL`, invisibly. It is used for its
-#' side-effects:
+#' side-effects of simultaneously
 #'
-#'   * it writes a Portable Translator File to `path`, and
-#'   * it writes further Portable Translations File(s) in the same directory
-#'     as `path`. One file per non-source native language defined in `x` is
-#'     created. Therefore, you may start supporting a new language by simply
-#'     adding a new entry to [`Translator$native_languages`][Translator].
+#'   * creating a Portable Translator file to the location given by `path`, and
+#'   * creating further Portable Translations file(s) in the same directory as
+#'     the Portable Translator file (by default).
+#'
+#' Note that underlying entries of `Translations Files` (a standard field of
+#' Portable Translator objects) may be modified afterwards. The file paths it
+#' contains are cached by [translations_read()], and reused automatically.
 #'
 #' [translations_read()] returns an S3 object of class
-#' [`ExportedTranslations`][export()]. Consider using [translator_read()]
-#' instead.
+#' [`ExportedTranslations`][export()].
 #'
 #' [translations_write()] returns `NULL`, invisibly. It is used for its
-#' side-effect of creating a single Portable Translations File.
+#' side-effect of creating a single Portable Translations file to the location
+#' given by `path`.
 #'
 #' @seealso
 #' [`Translator`][Translator],
-#' [export()]
+#' [serialize()]
 #'
 #' @examples
 #' # In what follows, ASCII characters are preferred because R has poor
-#' # support for non-ASCII characters used in man pages, and they must
-#' # be used cautiously. In practice, any alphabet (any UTF-8 character)
-#' # may be used to represent native languages and translations.
+#' # support for non-ASCII characters used in documentation (help pages),
+#' # and they must be used cautiously. In practice, any alphabet (any UTF-8
+#' # character) may be used to represent native languages and translations.
 #'
 #' # Set source language.
 #' language_source_set("en")
 #'
-#' # Define a location where Portable Translator Files are written.
-#' temp_path <- tempfile(pattern = "_translator_", fileext = ".yml")
+#' # Create a path to a (future) Portable Translator file.
+#' temp_path <- tempfile(pattern = "translator_", fileext = ".yml")
 #'
 #' # Create a Translator object.
 #' # This would normally be done automatically
@@ -144,10 +117,10 @@
 #' # Export it.
 #' # This creates 3 files:
 #' #
-#' #   1. 1 Portable Translator File for the Translator object, and
-#' #   2. 2 Portable Translations Files (one for each non-source language).
-#' #      The file for language "es" contains placeholders for future
-#' #      translations.
+#' #   1. 1 Portable Translator file, and
+#' #   2. 2 Portable Translations files (one for language "es", and another
+#' #      one for language "fr"). The file for language "es" contains
+#' #      placeholders for future translations.
 #' translator_write(tr, temp_path)
 #' translator_read(temp_path)
 #'
@@ -157,8 +130,6 @@
 #' cat(readLines(file.path(dirname(temp_path), "es.txt")), sep = "\n")
 #'
 #' # Translations can be read individually.
-#' # They are serialized as a YAML string
-#' # (on-the-fly) before being displayed.
 #' translations_read(file.path(dirname(temp_path), "fr.txt"))
 #' translations_read(file.path(dirname(temp_path), "es.txt"))
 #'
@@ -168,9 +139,9 @@
 #' #
 #' #   1. it has a corresponding entry in the underlying Translator object, and
 #' #   2. it has corresponding entries in the underlying Portable Translator
-#' #      File (fields languages and `Translations Files` must be updated).
+#' #      (fields 'Languages' and 'Translations Files' must be updated).
 #' #
-#' # Users should always used translator_write() instead.
+#' # Consider using translator_write() instead.
 #' tr$set_native_languages(el = "Greek")
 #' translations_write(tr, file.path(dirname(temp_path), "el.txt"), "el")
 #' translations_read(file.path(dirname(temp_path), "el.txt"))
@@ -181,9 +152,8 @@ translator_read <- function(
     path     = getOption("transltr.default.path"),
     encoding = "UTF-8")
 {
-    if (!is_translator(tr <- deserialize(text_read(path, encoding)))) {
-        stops("'path' must refer to a text file containing a serialized 'Translator' object.")
-    }
+    string <- paste0(text_read(path, encoding), collapse = "\n")
+    tr     <- deserialize(string)
 
     # Read all (Portable) Translations Files,
     # import them, and register translations.
@@ -197,36 +167,60 @@ translator_read <- function(
 #' @rdname translator-io
 #' @export
 translator_write <- function(
-    tr   = translator(),
-    path = getOption("transltr.default.path"))
+    tr        = translator(),
+    path      = getOption("transltr.default.path"),
+    overwrite = FALSE)
 {
-    if (!is_translator(tr)) {
-        stops("'tr' must be a 'Translator' object.")
+    # tr object is validated below by call to
+    # translations_files(). It requires a valid
+    # path, so it is thoroughly checked before.
+    assert_chr1(path)
+    assert_lgl1(overwrite)
+
+    path_abs     <- normalizePath(path, mustWork = FALSE)
+    path_dir     <- dirname(path)
+    path_dir_abs <- dirname(path_abs)
+
+    if (!overwrite && file.exists(path_abs)) {
+        # Path is shown instead of its
+        # absolute/canonical equivalent
+        # to avoid confusion.
+        stopf(
+            "'path' ('%s') already exists. Set 'overwrite' equal to 'TRUE' before proceeding.",
+            path)
+    }
+    if (!dir.exists(path_dir_abs) &&
+        !dir.create(path_dir_abs, TRUE, TRUE) || .__LGL_DEBUG_FLAG) {
+        # Absolute path to directory is shown,
+        # because it likely will be required
+        # for debugging purposes.
+        stopf(
+            "parent directory of path ('%s') could not be created. %s",
+            "Create it manually, or change 'path'.",
+            path_dir_abs)
     }
 
-    files    <- translations_files(tr)
-    path_dir <- create_parent_dir(path)
-    comments <- c(
-        "# Portable Translator File",
+    transl_paths <- translations_files(tr, path_dir)
+    comments     <- c(
+        "# Portable Translator",
         "#",
         "# Instructions:",
         "#  - You may edit Identifier, Languages, and Translations Files.",
         "#  - Do not edit other fields by hand; edit source scripts instead.",
         "#  - Field _Uuid uniquely identifies underlying objects.",
         "#  - Translations are stored in distinct Portable Translations Files.",
-        "#  - File paths listed under Translations Files must be relative to this file.",
         "%YAML 1.1",
         "---")
 
     # Write the Portable Translator File.
-    text_write(c(comments, serialize(tr)), path)
+    text_write(c(comments, serialize(tr, parent_dir = path_dir)), path)
 
     # Write Portable Translations files (one
     # per defined native language). They are
     # written in the same directory as path.
     map(translations_write,
-        path = file.path(path_dir, files),
-        lang = names(files),
+        path = transl_paths,
+        lang = names(transl_paths),
         more = list(tr = tr))
 
     return(invisible())
@@ -243,20 +237,20 @@ translations_read <- function(path = "", encoding = "UTF-8", tr) {
 #' @export
 translations_write <- function(tr = translator(), path = "", lang = "") {
     comments <- c(
-        "# Portable Translations File",
+        "# Portable Translations",
         "#",
         "# Instructions:",
         "#  - Edit each 'Translation' section below.",
         "#    - Translate what is in each preceding 'Source Text' section.",
         "#    - Do not edit other sections.",
-        "#    - Split long sentences. Single new lines are treated as spaces.",
         "#  - Choose UTF-8 whenever you have to select a character encoding.",
         "#  - You may use any text editor.",
+        "#  - You may split long sentences with single new lines.",
         "#  - You may include comments.",
         "#    - What follows an octothorpe (#) is ignored until the next line.",
         "#    - An escaped octothorpe (\\#) is treated as normal text.",
-        "#  - Developers may split long sentences into a multi-line string.",
         "")
 
-    return(text_write(c(comments, serialize_translations(tr, lang)), path))
+    text_write(c(comments, serialize_translations(tr, lang)), path)
+    return(invisible())
 }
