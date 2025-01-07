a71ecb6336bb426486378c2a023af9963548f04b
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-01-07 17:08:56 -0500

    Reintroduce importFrom() for R6::R6Class().
    R CMD check now throws an error again (it did not previously). I may have forgotten to regenerate the NAMESPACE.

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,8 @@
         # Default path to the main Exported Translator file.
         transltr.default.path = file.path("inst", "transltr", "_translator.yml"))
 }
+
+
+# Suppress R CMD check note.
+#' @importFrom R6 R6Class
+NULL
