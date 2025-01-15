94d6f8b4c68a8643e140dde7eff28b53a265cc40
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-01-15 14:51:06 -0500

    Modify description of parameter verbose.

--- man-roxygen/param-verbose.R
+++ man-roxygen/param-verbose.R
@@ -1,2 +1,2 @@
-#' @param verbose A non-[NA][base::NA] logical value. Should basic information
-#'   on extracted source texts be outputted?
+#' @param verbose A non-[NA][base::NA] logical value. Should progress
+#'   information be reported?
