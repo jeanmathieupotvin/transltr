1583a7ee1399dae22d5e5e6ae828339b59bcbcd4
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-02-14 08:40:56 -0500

    Bugfix: R 4.3 is required to use |> placeholders.

--- .scripts/create-description.R
+++ .scripts/create-description.R
@@ -38,9 +38,9 @@ usethis::use_description(
             translatable strings. Provide translations and seamlessly retrieve
             them at runtime."))
 
-# R >= 4.1.0 is required for pipe operator |> and function shorhand \(...).
+# R >= 4.3.0 is required for |>, _, and \(...).
 # R >= 4.2.0 is required for native UTF-8 support on Windows.
-use_package("R", type = "Depends", min_version = "4.2")
+use_package("R", type = "Depends", min_version = "4.3")
 
 usethis::use_testthat(parallel = FALSE)
 usethis::use_package("digest")
