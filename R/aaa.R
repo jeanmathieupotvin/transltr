618f61ea77a833d30b7b6224a3bb837962521bdd
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-03-21 09:27:43 -0400

    Start deprecating all internal constants.
    They will be replaced over time by dedicated functions returning
    them (and further information).

--- R/aaa.R
+++
@@ -1,5 +0,0 @@
-# An internal logical value always equal to `FALSE` used to force errors that
-# are hard, or (almost) impossible to test otherwise. It is only temporarily
-# set equal to `TRUE` via [testthat::with_mocked_bindings()].
-.__LGL_DEBUG_FLAG <- FALSE
-.__STR_UNSET      <- "<unset>"
